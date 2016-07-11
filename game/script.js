d95f1aa3f2bac30c59b1a6901f0ecd36c0e68b90
Alex <thisisbanana@hotmail.com>
2016-07-12 00:49:41 +1000

    Minor changes

--- game/script.js
+++ game/script.js
@@ -1,6 +1,6 @@
 const tileSize = 100, borderWidth = 5, margin = 1;
 var space = {"col": 0, "row": 0, "number": 0};
-var valid = false;
+var valid = isAnimating = false;
 
 function getTopPos(id, n) {
     return (tileSize + 2 * borderWidth) * Math.floor(id / n);
@@ -49,10 +49,10 @@ function Field() {
         for (let i = 0; i < n; ++i) {
             f.appendChild(createDiv("row", "row" + i));
             for (let j = 0; j < n; ++j)
-                f.lastChild.appendChild(createDiv("tile", (i + 1) + j * n, tileMove, (i + 1) + j * n));
+                f.lastChild.appendChild(createDiv("tile", i * n + j, tileMove, (i + 1) + j * n));
         }
-        with (f.lastChild.lastChild) { className = "space"; id = "space"; };
-        space.number = f.lastChild.previousSibling.lastChild.id*1 + 1;
+        with (f.lastChild.lastChild) { className = "space"; id = "space"; }
+        space.number = Math.pow(document.getElementById("dimension").value, 2) - 1//f.lastChild.previousSibling.lastChild.id*1 + 1;
         space.col = space.row = n-1;
     };
 }
@@ -70,39 +70,51 @@ function findTile(id) {
 }
 
 function tileMove() {
-    if (valid) {
+    if (valid && !isAnimating) {
+        isAnimating = true;
         let spaceTile = findTile("space");
         let tileId = this.id, spaceId = "space";
         let tileClassName = "tile", spaceClassName = "space";
         let tileLabel = this.lastChild.innerHTML, spaceLabel = spaceTile.lastChild.innerHTML;
 
-        this.id = spaceId; spaceTile.id = spaceLabel;
+        this.id = spaceId; spaceTile.id = space.number;
         this.className = spaceClassName; spaceTile.className = tileClassName;
-        this.lastChild.innerHTML = spaceTile.lastChild.innerHTML = tileLabel;
+        this.lastChild.innerHTML = spaceLabel;
+        spaceTile.lastChild.innerHTML = tileLabel;
+        space.number = parseInt(tileId);
+    }
+}
 
-        space.number = parseInt(tileLabel);
-    };
+function div(x, y) {
+    "use strict";
+    return Math.floor(x / y);
 }
 
 function tileHover() {
     if (this.id == "space") return;
-    field.setTileColor(this.style.backgroundColor);
+    //field.setTileColor(this.style.backgroundColor);
     let id = parseInt(this.id);
     let n = document.getElementById("dimension").value*1;
-    if ((id + 1 == space.number) || (id - 1 == space.number) || (id + n == space.number) || (id - n == space.number)) {
+    if ((id + 1 == space.number && div(id + 1, n) == div(space.number, n)) || 
+        (id - 1 == space.number && div(id - 1, n) == div(space.number, n)) || 
+        (id + n == space.number) || (id - n == space.number)) {
         valid = true;
-        this.style.backgroundColor = "#00B16A";
+        this.className = "tile-near-space";
     }
+    isAnimating = false;
 }
 
 function tileHout() {
     if (this.id == "space") return;
     let id = parseInt(this.id);
-    let n = document.getElementById("dimension").value*1;
-    if ((id + 1 == space.number) || (id - 1 == space.number) || (id + n == space.number) || (id - n == space.number)) {
+    let n = parseInt(document.getElementById("dimension").value);
+    if ((id + 1 == space.number && div(id + 1, n) == div(space.number, n)) ||
+        (id - 1 == space.number && div(id - 1, n) == div(space.number, n)) ||
+        (id + n == space.number) || (id - n == space.number)) {
         valid = false;
-        this.style.backgroundColor = field.getTileColor();
+        this.className = "tile";
     }
+    isAnimating = false;
 }
 
 function initialize() {
