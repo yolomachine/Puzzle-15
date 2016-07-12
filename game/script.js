38e09d408b5ec72b3f9470e2fb8d666b1637d51b
Alex <thisisbanana@hotmail.com>
2016-07-12 11:52:00 +1000

    Minor changes

--- game/script.js
+++ game/script.js
@@ -1,13 +1,7 @@
-const tileSize = 100, borderWidth = 5, margin = 1;
-var space = {"col": 0, "row": 0, "number": 0};
-var valid = isAnimating = false, selected = "li4";
+var valid = false, isAnimating = false, selected = "li4", space = {"number": 0, "className": "space", "id": "space"};
 
-function getTopPos(id, n) {
-    return (tileSize + 2 * borderWidth) * Math.floor(id / n);
-}
-
-function getLeftPos(id, n) {
-    return (tileSize + 2 * borderWidth) * (id - Math.floor(id / n) * n);
+function div(x, y) {
+    return Math.floor(x / y);
 }
 
 function createDiv(className, id, fn, text) {
@@ -16,8 +10,8 @@ function createDiv(className, id, fn, text) {
     div.id = id;
     if (className == "tile") {
         div.onclick = fn;
-        div.onmouseover = tileHover;
-        div.onmouseout  = tileHout;
+        div.onmouseover = tileNearSpaceHover;
+        div.onmouseout  = tileNearSpaceHout;
         let label = document.createElement("label");
         label.innerHTML = text;
         div.appendChild(label);
@@ -29,8 +23,22 @@ function clear(obj) {
     while (obj.lastChild) obj.removeChild(obj.lastChild);
 }
 
-function animate() {
+function classSwap(a, b) {
+    let temp = a.className;
+    a.className = b.className;
+    b.className = temp;
+}
 
+function idSwap(a, b) {
+    let temp = a.id;
+    a.id = b.id;
+    b.id = temp;
+}
+
+function labelSwap(a, b) {
+    let temp = a.lastChild.innerHTML;
+    a.lastChild.innerHTML = b.lastChild.innerHTML;
+    b.lastChild.innerHTML = temp;
 }
 
 function Field() {
@@ -41,17 +49,47 @@ function Field() {
         for (let i = 0; i < n; ++i) {
             f.appendChild(createDiv("row", "row" + i));
             for (let j = 0; j < n; ++j)
-                f.lastChild.appendChild(createDiv("tile", i * n + j, tileMove, (i + 1) + j * n));
+                f.lastChild.appendChild(createDiv("tile", i * n + j, moveTile, (i + 1) + j * n));
         }
-        with (f.lastChild.lastChild) { className = "space"; id = "space"; }
+        with (f.lastChild.lastChild) { className = space.className; id = space.id; }
         space.number = Math.pow(document.getElementById(selected).innerHTML*1, 2) - 1
-        space.col = space.row = n-1;
     };
+    this.createLevelSelectors = function() {
+        let ul = document.getElementById("selector");
+        for (let x, i = 3; i < 8; ++i) {
+            x = document.createElement("li");
+            x.className = "li-not-selected";
+            x.id = "li" + i;
+            x.innerHTML = i;
+            x.onclick = this.setLevel;
+            ul.appendChild(x);
+        }
+        document.getElementById(selected).className = "li-selected";
+    }
+    this.setLevel = function() {
+        classSwap(this, document.getElementById(selected));
+        idSwap(this, document.getElementById(selected));
+        field.generateLevel();
+    }
     this.shuffle = function() {
         for (let i = 1, a, b; i < Math.pow(document.getElementById(selected).innerHTML*1, 2) - 1; ++i) {
-            a = findTile(i); do { b = findTile(Math.floor(Math.random() * i)) } while(b.className == "space");
+            a = findTile(i); do { b = findTile(Math.floor(Math.random() * i)) } while(b.className == space.className);
             labelSwap(a, b);
         }
+    }
+    this.checkAnswer = function() {
+        let n = parseInt(document.getElementById(selected).innerHTML);
+        let row = document.getElementById("field").firstChild;
+        for (let i = 0; i < n; ++i) {
+            let child = row.firstChild;
+            for (let j = 0; j < n; ++j) {
+                if ((i + 1) + j * n != child.lastChild.innerHTML) return;
+                child = child.nextSibling;
+            }
+            row = row.nextSibling;
+        }
+        alert("You won!");
+        field.shuffle();
     }
 }
 
@@ -67,36 +105,7 @@ function findTile(id) {
     }
 }
 
-function setLevel() {
-    "use strict";
-    classSwap(this, document.getElementById(selected));
-    idSwap(this, document.getElementById(selected));
-    field.generateLevel();
-}
-
-function classSwap(a, b) {
-    "use strict";
-    let temp = a.className;
-    a.className = b.className;
-    b.className = temp;
-}
-
-function idSwap(a, b) {
-    "use strict";
-    let temp = a.id;
-    a.id = b.id;
-    b.id = temp;
-}
-
-function labelSwap(a, b) {
-    "use strict";
-    let temp = a.lastChild.innerHTML;
-    a.lastChild.innerHTML = b.lastChild.innerHTML;
-    b.lastChild.innerHTML = temp;
-}
-
-function tileSwap(a, b) {
-    "use strict";
+function swapTile(a, b) {
     let aId = a.id, bId = b.id,
         aClassName = a.className = "tile",
         bClassName = b.className,
@@ -111,20 +120,16 @@ function tileSwap(a, b) {
     space.number = parseInt(aId);
 }
 
-function tileMove() {
+function moveTile() {
     if (valid && !isAnimating) {
         isAnimating = true;
-        tileSwap(this, findTile("space"));
+        swapTile(this, findTile(space.id));
+        field.checkAnswer();
     }
 }
 
-function div(x, y) {
-    "use strict";
-    return Math.floor(x / y);
-}
-
-function tileHover() {
-    if (this.id == "space") return;
+function tileNearSpaceHover() {
+    if (this.id == space.id) return;
     let id = parseInt(this.id);
     let n = document.getElementById(selected).innerHTML*1;
     if ((id + 1 == space.number && div(id + 1, n) == div(space.number, n)) || 
@@ -136,10 +141,10 @@ function tileHover() {
     isAnimating = false;
 }
 
-function tileHout() {
-    if (this.id == "space") return;
+function tileNearSpaceHout() {
+    if (this.id == space.id) return;
     let id = parseInt(this.id);
-    let n = parseInt(document.getElementById(selected).innerHTML);
+    let n  = parseInt(document.getElementById(selected).innerHTML);
     if ((id + 1 == space.number && div(id + 1, n) == div(space.number, n)) ||
         (id - 1 == space.number && div(id - 1, n) == div(space.number, n)) ||
         (id + n == space.number) || (id - n == space.number)) {
@@ -150,16 +155,7 @@ function tileHout() {
 }
 
 function initialize() {
-    let ul = document.getElementById("selector");
-    for (let x, i = 3; i < 8; ++i) {
-        x = document.createElement("li");
-        x.className = "li-not-selected";
-        x.id = "li" + i;
-        x.innerHTML = i;
-        x.onclick = setLevel;
-        ul.appendChild(x);
-    }
-    document.getElementById(selected).className = "li-selected";
+    field.createLevelSelectors();
     field.generateLevel();
 }
 
